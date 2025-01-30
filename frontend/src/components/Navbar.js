9c23a63025c3f13b498b6d7fb04f34af255cb2b3
23003793 <23003793@myrp.edu.sg>
2025-01-30 18:12:32 +0800

    NA

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,41 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const Navbar = () => {
 
-  return (
-    <header>
-      <div className="container">
-        <Link to="/">
-          <h1>Workout Buddy</h1>
-        </Link>
-      </div>
-    </header>
-  )
+    const { logout } = useLogout();
+
+    const { user } = useAuthContext();
+
+    const handleClick = () => {
+        logout();
+    }
+
+    return (
+        <header>
+            <div class="container">
+                <Link to="/">
+                <h1>Welcome to Workout Buddy!💪🏻</h1>
+                </Link>
+
+                <nav>
+                    {user && (
+                        <div>
+                            <span>{user.email}</span>
+                            <button onClick={handleClick}>Log Out</button>
+                        </div>
+                    )}
+                    { !user && (
+                        <div>
+                            <Link to="/login">Log In</Link>
+                            <Link to="/signup">Sign Up</Link>
+                        </div>
+                    )}
+                </nav>
+            </div>
+        </header>
+    )
 }
 
-export default Navbar
+export default Navbar;
--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,31 +1,124 @@
-import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useState } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // date fns
-import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 const WorkoutDetails = ({ workout }) => {
-  const { dispatch } = useWorkoutsContext()
 
-  const handleClick = async () => {
-    const response = await fetch('/api/workouts/' + workout._id, {
-      method: 'DELETE'
-    })
-    const json = await response.json()
+    const { dispatch } = useWorkoutsContext();
+    const { user } = useAuthContext();
 
-    if (response.ok) {
-      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedWorkout, setEditedWorkout] = useState({
+        title: workout.title,
+        load: workout.load,
+        reps: workout.reps
+    });
+
+    const handleClick = async () => {
+        if (!user) {
+            return
+        }
+
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workouts/${workout._id}`, {
+            method: 'DELETE',
+            headers: {
+                "Authorization": `Bearer ${user.token}`
+            }
+        });
+
+        const json = await response.json();
+
+        if (response.ok) {
+            dispatch({type: 'DELETE_WORKOUT', payload: json});
+        }
     }
-  }
+    
+    const handleEditClick = () => {
+        setIsEditing(true);
+    }
 
-  return (
-    <div className="workout-details">
-      <h4>{workout.title}</h4>
-      <p><strong>Load (kg): </strong>{workout.load}</p>
-      <p><strong>Number of reps: </strong>{workout.reps}</p>
-      <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
-      <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
-    </div>
-  )
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setEditedWorkout({
+            ...editedWorkout,
+            [name]: value
+        });
+    }
+
+    const handleSubmit = async () => {
+
+        if (!user) {
+            return
+        }
+        
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workouts/${workout._id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": `Bearer ${user.token}`
+            },
+            body: JSON.stringify(editedWorkout)
+        });
+
+        const json = await response.json();
+
+        if (response.ok) {
+            dispatch({type: 'UPDATE_WORKOUT', payload: json});
+            setIsEditing(false);
+        }
+    }
+
+    return (
+        // <div className="workout-details">
+        //     <h4>{workout.title}</h4>
+        //     <p><strong>Load (kg): </strong>{workout.load}</p>
+        //     <p><strong>Reps: </strong>{workout.reps}</p>
+        //     <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true})}</p>
+            
+        //     <span className="material-symbols-outlined edit-icon">edit</span>
+        //     <span className="material-symbols-outlined delete-icon" onClick={handleClick}>delete</span>
+        // </div>
+
+        <div className="workout-details">
+            {isEditing ? (
+                <div>
+                    <input
+                        type="text"
+                        name="title"
+                        value={editedWorkout.title}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="number"
+                        name="load"
+                        value={editedWorkout.load}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="number"
+                        name="reps"
+                        value={editedWorkout.reps}
+                        onChange={handleChange}
+                    />
+                    <button className="btn" onClick={handleSubmit}>Save</button>
+                    <button className="btn cancel" onClick={() => setIsEditing(false)}>Cancel</button>
+                </div>
+            ) : (
+                <>
+                    <h4>{workout.title}</h4>
+                    <p><strong>Load (kg): </strong>{workout.load}</p>
+                    <p><strong>Reps: </strong>{workout.reps}</p>
+                    <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
+
+                    <span className="material-symbols-outlined edit-icon" onClick={handleEditClick}>edit</span>
+                    <span className="material-symbols-outlined delete-icon" onClick={handleClick}>delete</span>
+                </>
+            )}
+        </div>
+    )
 }
 
-export default WorkoutDetails
+export default WorkoutDetails;
